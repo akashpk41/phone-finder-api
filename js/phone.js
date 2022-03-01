ec89c20f6e1b4fe8cae9651dfecb26330e94f3f3
akashpk41 <akashpk84100@gmail.com>
2022-03-01 17:55:20 +0600

    some code fixed

--- js/phone.js
+++ js/phone.js
@@ -1,44 +1,41 @@
 const searchPhone = () => {
-  const searchField = document.getElementById('search-field');
+  const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
-  console.log(searchText);
   //! clear search field.
-  searchField.value = ''
+  searchField.value = "";
 
   // ! condition for search feild
-  if( searchText == ''){
-    alert('Write Something To See Something!')
-  };
+  if (searchText == "") {
+    alert("Write Something To See Something!");
+  }
   // ! working on api ,
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
   fetch(url)
-  .then(res => res.json())
-  .then(data => displaySearchResult(data.data.slice(0,20)) );
+    .then((res) => res.json())
+    .then((data) => displaySearchResult(data.data.slice(0, 20)));
   // ! loading speaner
-  loadingSpinner('block');
-
-
-}
+  loadingSpinner("block");
+};
 
 // ! function for show loading when something search.
-const loadingSpinner = style => {
-  document.getElementById('loading').style.display = style;
-}
+const loadingSpinner = (style) => {
+  document.getElementById("loading").style.display = style;
+};
 
 // ! function for display search result into ui
-const displaySearchResult = phones => {
+const displaySearchResult = (phones) => {
   // ! add one condition, if no phone found.
-  if( phones.length == [] ){
-    document.getElementById('not-available').style.display = 'block';
-    loadingSpinner('none')
+  if (phones.length == []) {
+    document.getElementById("not-available").style.display = "block";
+    loadingSpinner("none");
   }
-  const phonesContainer = document.getElementById('phones');
-  phonesContainer.textContent = ''
-  phones.map( phone => {
+  const phonesContainer = document.getElementById("phones");
+  phonesContainer.textContent = "";
+  phones.map((phone) => {
     // console.log(phone);
     //! creating new element to show search result in the html page.
-    const div = document.createElement('div');
-    div.classList.add('col');
+    const div = document.createElement("div");
+    div.classList.add("col");
     div.innerHTML = `
     <div class="card h-100 border-0 rounded-3 shadow-lg ">
     <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
@@ -49,41 +46,42 @@ const displaySearchResult = phones => {
     </div>
   </div>
     `;
-    phonesContainer.appendChild(div)
+    phonesContainer.appendChild(div);
     // ! hide spinner when search result show
-    loadingSpinner('none');
-    document.getElementById('not-available').style.display = 'none';
-
-  })
-
-}
+    loadingSpinner("none");
+    document.getElementById("not-available").style.display = "none";
+  });
+};
 
 // ! function for load  phone details from api
-const loadPhoneDetails = phoneId => {
+const loadPhoneDetails = (phoneId) => {
   // console.log(phoneId);
   const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
   fetch(url)
-  .then(res => res.json())
-  .then( data => showPhoneDetails(data.data))
-}
+    .then((res) => res.json())
+    .then((data) => showPhoneDetails(data.data));
+};
 
 // ! function for show phone details
-const showPhoneDetails = phoneDetails => {
-
-  const phoneDetailsContainer = document.getElementById('phone-details');
+const showPhoneDetails = (phoneDetails) => {
+  const phoneDetailsContainer = document.getElementById("phone-details");
   // ! clear previous details
-  phoneDetailsContainer.textContent = ''
+  phoneDetailsContainer.textContent = "";
   //! creating new element for show phone details in html page
 
-  const div = document.createElement('div');
-  div.classList.add('card');
+  const div = document.createElement("div");
+  div.classList.add("card");
   div.innerHTML = `
 
   <img src="${phoneDetails.image}" class="card-img-top" alt="..." />
   <div class="card-body text-center">
     <h3 class="card-title fw-bold">${phoneDetails.name}</h3>
     <h4 class="card-title text-primary">${phoneDetails.brand}</h4>
-    <p class="card-title fw-bold ">Release Date: ${phoneDetails.releaseDate} </p>
+    <p class="card-title fw-bold ">Release Date: ${
+      phoneDetails.releaseDate
+        ? phoneDetails.releaseDate
+        : "No released date found."
+    } </p>
 
     <!-- ! main features -->
     <ul class="list-group">
@@ -91,13 +89,19 @@ const showPhoneDetails = phoneDetails => {
         Main Features
       </li>
       <li class="list-group-item text-start">
-        <span class="fw-bold">Storage:</span> ${phoneDetails.mainFeatures.storage}",
+        <span class="fw-bold">Storage:</span> ${
+          phoneDetails.mainFeatures.storage
+        }",
       </li>
       <li class="list-group-item list-group-item-dark text-start">
-        <span class="fw-bold">Display Size:</span> ${phoneDetails.mainFeatures.displaySize}
+        <span class="fw-bold">Display Size:</span> ${
+          phoneDetails.mainFeatures.displaySize
+        }
       </li>
       <li class="list-group-item text-start">
-        <span class="fw-bold">Chip Set:</span> ${phoneDetails.mainFeatures.chipSet}
+        <span class="fw-bold">Chip Set:</span> ${
+          phoneDetails.mainFeatures.chipSet
+        }
       </li>
       <li class="list-group-item list-group-item-dark text-start">
         <span class="fw-bold">Memory:</span> ${phoneDetails.mainFeatures.memory}
@@ -140,5 +144,5 @@ const showPhoneDetails = phoneDetails => {
     </ul>
   </div>
   `;
-  phoneDetailsContainer.appendChild(div)
-}
+  phoneDetailsContainer.appendChild(div);
+};
