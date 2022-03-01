5b717b2287accd7cf30d50a6863eecc34eabe43b
akashpk41 <akashpk84100@gmail.com>
2022-03-01 06:50:13 +0600

    html markup complete, js file added and one condition added in js file

--- js/phone.js
+++ js/phone.js
@@ -1,1 +1,13 @@
-console.log('script connected');
+const searchPhone = () => {
+  const searchField = document.getElementById('search-field');
+  const searchText = searchField.value;
+  console.log(searchText);
+  //! clear search field.
+  searchField.value = ''
+
+  // ! condition for search feild
+  if( searchText == ''){
+    alert('Write Something To See Something!')
+  }
+  
+}
