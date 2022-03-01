2fb3aebf8b9a032bdb3e8fcafc5b9c0273a0dc3b
akashpk41 <akashpk84100@gmail.com>
2022-03-01 07:16:39 +0600

    search result showing into ui successfully

--- js/phone.js
+++ js/phone.js
@@ -8,6 +8,37 @@ const searchPhone = () => {
   // ! condition for search feild
   if( searchText == ''){
     alert('Write Something To See Something!')
-  }
-  
+  };
+  // ! working on api ,
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displaySearchResult(data.data.slice(0,20)) )
+
+}
+// ! function for display search result into ui
+const displaySearchResult = phones => {
+  // console.log(phones.slice(0,20));
+  const phonesContainer = document.getElementById('phones');
+  phonesContainer.textContent = ''
+  phones.map( phone => {
+    // console.log(phone);
+    //! creating new element to show search result in the html page.
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
+    <div class="card h-100 border-0 rounded-3 shadow-lg ">
+    <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
+    <div class="card-body text-center ">
+      <h4 class="card-title fw-bold "> Model: ${phone.phone_name}</h4>
+      <h5 class="card-title text-primary "> Brand: ${phone.brand}</h5>
+      <button class="btn btn-outline-primary mt-3 "> More Details </button>
+    </div>
+  </div>
+    `;
+    phonesContainer.appendChild(div)
+
+
+  })
+
 }
